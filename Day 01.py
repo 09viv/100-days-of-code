09109631f0e175102ef4cb88d5a3832aeca8e083
09viv <114340781+09viv@users.noreply.github.com>
2024-02-07 14:46:02 +0530

    Add files via upload

---
+++ Day 01.py
@@ -0,0 +1,5 @@
+username = input('''Welcome to band name project.
+                 Enter your name: ''')
+petname = input("Enter your pet name: ")
+bandname = username +" " + petname
+print(bandname)
